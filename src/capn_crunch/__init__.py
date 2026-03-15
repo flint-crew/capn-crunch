175f577ad304a8aad634b19c5e7b94cdbd397b79
Alec Thomson <alec.thomson@skao.int>
2026-03-15 17:19:51 +0800

    First commit

---
+++ src/capn_crunch/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from .core import BaseOptions, add_options_to_parser, create_options_from_parser
+
+__all__ = ["BaseOptions", "add_options_to_parser", "create_options_from_parser"]
