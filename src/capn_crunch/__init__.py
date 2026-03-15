b658e3d851fb86770bd9a913c2da0ec6f14f908f
Alec Thomson <alec.thomson@skao.int>
2026-03-15 18:37:15 +0800

    Expose all funcs

--- src/capn_crunch/__init__.py
+++ src/capn_crunch/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import annotations
 
-from .core import BaseOptions, add_options_to_parser, create_options_from_parser
+from .core import BaseOptions, add_options_to_parser, create_options_from_parser, options_to_dict
 
-__all__ = ["BaseOptions", "add_options_to_parser", "create_options_from_parser"]
+__all__ = ["BaseOptions", "add_options_to_parser", "create_options_from_parser", "options_to_dict"]
